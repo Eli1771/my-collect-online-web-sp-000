4ba986d8a37b5eff3910ca0ba826ffe73926c26f
elijahmm17 <elijahmm17@gmail.com>
2019-05-15 19:16:55 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(collection)
   i = 0 
   new_collection = []
   while i < collection.size do
-    new_collection << yeild(collection[i])
+    new_collection << yield(collection[i])
     i += 1 
   end
   new_collection
